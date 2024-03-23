d3d5a83054c4f0db1b3e670f13e508cc8b8f59d3
Amir-Alam01 <sm.amiralam01@yahoo.com>
2024-03-23 19:22:45 +0500

    push my calculator to github

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+import inquirer from "inquirer";
 const answer = await inquirer.prompt([
   { message: "Enter your first number", type: "number", name: "firstNumber" },
   { message: "Enter your second number", type: "number", name: "secondNumber" },
@@ -23,4 +24,3 @@ if (answer.operator === "Addition") {
 } else {
     console.log("Please select valid operator")
 }
-console.log("THE END")
